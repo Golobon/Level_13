c67bf58f138fc189f38c110c350dc77fa1a262e0
Golobon <golobon@ya.ru>
2022-08-12 00:04:27 +0300

    Commit

--- src/Lecture_2/Solution2.java
+++ src/Lecture_2/Solution2.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 public class Solution2 {
     public static void main(String[] args) throws Exception {
         ArrayList<String> list = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 10; i++) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             String iSR = bufferedReader.readLine();
             list.add(0, iSR);
         }
