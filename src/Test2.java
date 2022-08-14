10383853ce6ff6a25144983647f4e9441b61cfbe
Golobon <golobon@ya.ru>
2022-08-14 22:01:21 +0300

    Commit

---
+++ src/Test2.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List <String> list = new ArrayList();
+        list.add(String.valueOf(2222));
+        list.add("3");
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        printstring(String.valueOf(list.get(0)));
+        System.out.println(list.size());
+    }
+
+    public static void printstring(String list) {
+        System.out.println(list.length());
+    }
+}
