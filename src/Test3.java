518a1ae9cce9958743039a267efcdd6b73c85a7c
Golobon <golobon@ya.ru>
2022-08-22 13:08:22 +0300

    Commit

--- src/Test3.java
+++ src/Test3.java
@@ -1,35 +1,28 @@
-
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test3 {
-    private final String[] students = new String[30];
-
-    //static int i = 0;
-
     public static void main(String[] args) {
-        Test3 uni = new Test3();
-        uni.addStudent("Максим Федоренко");
-        uni.addStudent("Олег Кесарчук");
-        uni.addStudent("Олег Кесарчук");
-        uni.addStudent("Олег Кесарчук");
-        uni.addStudent("Олег Кесарчук");
-        uni.addStudent("Олег Кесарчук");
-        uni.addStudent("Олег Кесарчук");
-        uni.printStudents();
-    }
-
-    public void addStudent(String student) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] name;
+        int string;
+        System.out.println("Введите колличество строк двумерного массива");
+        string = scanner.nextInt();
+        System.out.println("Введите колличество столбцов двумерного массива");
+        int columns;
+        columns = scanner.nextInt();
+        name = new int[string][columns];
         int i = 0;
-        if (students[i++] == null) students[i] = student;
-    }
-
-    public void printStudents() {
-        for (String student : students) {
-            if (student != null) {
-                System.out.println(student);
-            }
+        int j = 0;
+        while (i<=string-1) {
+            System.out.println("Введи данные ячейки массива с индектом :" + "[" + i + "]" + "[" + j + "]");
+            name[i][j] = scanner.nextInt();
+            System.out.println("Ячейки массива с индексами :" + "[" + i + "]" + "[" + j + "]" + " присвоего знечение: " + name[i][j] + "\n");
+            if (j == columns-1) { System.out.println("Строка массива с индектом " + "[" + j + "]" + " заполнена." + "\n"); }
+            j++;
+            if (j == columns && i == string-1) {break;}
+            if (j == columns) {i++; j = 0;}
         }
+        System.out.println("Вывод данных двумерного массива на экран: " + (Arrays.deepToString(name)));
     }
-
-
 }
